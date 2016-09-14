7c941a288790ffd972288535cd47c4b3e09bef0d
Yanick Champoux <yanick@cpan.org>
2016-09-14 17:25:33 -0400

    Build results of 268db86 (on master)

--- lib/JSON/Schema/Shorthand.pm
+++ lib/JSON/Schema/Shorthand.pm
@@ -1,6 +1,7 @@
 package JSON::Schema::Shorthand;
+our $AUTHORITY = 'cpan:YANICK';
 # ABSTRACT: Alternative, condensed format for JSON Schemas
-
+$JSON::Schema::Shorthand::VERSION = '0.0.1';
 use strict;
 use warnings;
 
@@ -68,7 +69,20 @@ sub js_shorthand {
 }
 
 1;
+
 __END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+JSON::Schema::Shorthand - Alternative, condensed format for JSON Schemas
+
+=head1 VERSION
+
+version 0.0.1
 
 =head1 SYNOPSIS
 
@@ -87,7 +101,6 @@ __END__
     #  }
     #  required => [ 'bar' ],
     # }
-    
 
 =head1 DESCRIPTION
 
@@ -159,7 +172,6 @@ C<{ object: properties }> expands to C<{ type: "object", properties }>.
     }                                           }
                                             }
 
-
 =head3 C<array> property
 
 C<{ array: items }> expands to C<{ type: "array", items }>.
@@ -172,7 +184,6 @@ C<{ array: items }> expands to C<{ type: "array", items }>.
                                                     type: 'number' 
                                                 }
                                             }
-
 
 =head3 C<required> property
 
@@ -188,9 +199,6 @@ up to the C<required> attribute of its parent object.
           baz: { }                                bar: {},
         }                                         baz: {}
     }                                       }
-                                            
-
-
 
 =head1 SEE ALSO
 
@@ -198,5 +206,15 @@ up to the C<required> attribute of its parent object.
 
 * JavaScript version of this module - L<http://github.com/yanick/json-shema-shorthand>
 
+=head1 AUTHOR
 
+Yanick Champoux <yanick@babyl.dyndns.org>
 
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Yanick Champoux.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
